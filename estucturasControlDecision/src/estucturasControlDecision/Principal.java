09a80fbb67e5c1d361d9dcdf42cf69f058eac9f2
Riche2000 <ricardosanch2000@gmail.com>
2023-01-26 16:13:58 -0600

    Estructuras control

---
+++ estucturasControlDecision/src/estucturasControlDecision/Principal.java
@@ -0,0 +1,20 @@
+package estucturasControlDecision;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		//Estructuras control
+		int edad=19;
+		//Si edad es mayor a 18
+		if(edad>18) {
+			System.out.println("Tiene más de 18 años");
+		}
+		else if(edad>16) {
+			System.out.println("Tiene más de 16 años");
+		}
+		else {
+			System.out.println("Tiene menos de 16 años");
+		}
+
+	}
+}
