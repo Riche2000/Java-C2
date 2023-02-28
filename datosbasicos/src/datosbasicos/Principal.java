b0ce42592dc278778554aa954291f0530f28aa3f
Riche2000 <ricardosanch2000@gmail.com>
2023-02-28 10:44:29 -0600

    Datos basicos

---
+++ datosbasicos/src/datosbasicos/Principal.java
@@ -0,0 +1,21 @@
+package datosbasicos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// Variables numericas
+		byte numOrdenadores = 2;
+		System.out.println("El número de ordenadores es: "+numOrdenadores);
+		
+		int edadRicardo = 22;
+		System.out.println("Edad Ricardo "+edadRicardo);
+		
+		//para los float se usa la f al final
+		float temperatura=23.2f;
+		System.out.println("Temperatura: "+temperatura);
+		
+		String nombreAcademia="Mastermind";
+		System.out.println("La academia es: "+nombreAcademia);
+	}
+
+}
